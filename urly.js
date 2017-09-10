01701ff731e187f6315e42b5c77fb858727ed9bb
Calvin Iyer <calvin-iyer@users.noreply.github.com>
2017-09-10 21:23:34 +0530

    Update urly.js

--- urly.js
+++ urly.js
@@ -1,1 +1,8 @@
-
+function test() {
+	if(document.getElementById('skip_button2') == null || document.getElementById('skip_button2').tagName.toLowerCase() !== 'a') {
+		setTimeout(test,3000);
+    } else {
+		document.getElementById('skip_button2').click();
+    }
+}
+test();
