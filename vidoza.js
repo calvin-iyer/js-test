16b5ed9a39bd77395d6aaf3006e2d90da7a7f8b5
Calvin Iyer <calvin-iyer@users.noreply.github.com>
2017-10-20 18:03:39 +0530

    Update vidoza.js

--- vidoza.js
+++ vidoza.js
@@ -1,8 +1,9 @@
 function playVideo() {
   if(document.getElementsByClassName("jw-video jw-reset").length > 0) {
     document.getElementsByClassName("jw-video jw-reset")[0].play();
+    setTimeout(playVideo,5000);
   } else {
     setTimeout(playVideo,5000);
   }
 }
-setTimeout(playVideo,5000);
+playVideo();
