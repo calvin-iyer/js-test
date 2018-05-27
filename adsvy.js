188975660a4ab7dd0d0b9bfd2911bcab18586390
Calvin Iyer <calvin-iyer@users.noreply.github.com>
2018-05-27 14:30:15 +0530

    Update adsvy.js

--- adsvy.js
+++ adsvy.js
@@ -1,8 +1,13 @@
 function test() {
+if (document.readyState === "complete") {
 	if(document.getElementsByClassName('btn')[0].tagName.toLowerCase() !== 'a') {
 		setTimeout(test,3000);
     } else {
 		document.getElementsByClassName('btn')[0].click();
+		setTimeout(test,3000);
     }
+} else {
+	setTimeout(test,3000);
+}
 }
 test();
